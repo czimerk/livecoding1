0dc1cb8c57b8933fd72d2542c59d8c6bf2170a24
czimerk <czimer.kristof@gmail.com>
2021-02-20 22:49:35 +0100

    finishing up

--- LiveCoding.WebApp/ClientApp/src/app/calendar-event/calendar-event.component.ts
+++ LiveCoding.WebApp/ClientApp/src/app/calendar-event/calendar-event.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as moment from 'moment';
-
+import { Guid } from 'guid-typescript';
 @Component({
   selector: 'app-calendar-event',
   templateUrl: './calendar-event.component.html'
@@ -10,17 +10,20 @@ export class CalendarEventComponent {
   public calendarEvents: CalendarEvent[];
   currentEvent: CalendarEvent;
   isEditMode: boolean;
+  isAddMode: boolean;
 
-  impOptions:any[] = [
+  impOptions: any[] = [
     {
-      value:true,
-      name:"Yes"
+      value: true,
+      name: "Yes"
     },
     {
-      value:false,
-      name:"No"
+      value: false,
+      name: "No"
     }
   ]
+
+
   constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
     this.refreshTable();
   }
@@ -31,44 +34,86 @@ export class CalendarEventComponent {
     }, error => console.error(error));
   }
 
-  formatDate(date){
+  formatDate(date) {
     return date.split('T')[0];
   }
 
-  formatImportant(isImportant){
+  formatImportant(isImportant) {
     return isImportant ? "Yes" : "No";
   }
 
-  editEvent(calendarEvent){
-    if (this.isEditMode){
+  editEvent(calendarEvent) {
+    this.isAddMode = false;
+    if (this.isEditMode && calendarEvent.id === this.currentEvent.id) {
       this.isEditMode = false;
       this.currentEvent = null;
-    }else{
+    } else {
       this.isEditMode = true;
-      this.currentEvent = {...calendarEvent};
+      this.currentEvent = { ...calendarEvent };
     }
   }
 
-  ok(currentEvent){
-    if (currentEvent){
-      currentEvent.isImportant = currentEvent.isImportant === "true";
-      this.http.put<CalendarEvent[]>(this.baseUrl + 'api/calendarevent/' + currentEvent.id, currentEvent).subscribe(result => {
-        
-        if (result){
+  addEvent(calendarEvent) {
+    this.isEditMode = false;
+    if (this.isAddMode) {
+      this.isAddMode = false;
+      this.currentEvent = null;
+    } else {
+      this.isAddMode = true;
+      this.currentEvent = {
+        id: "",
+        name: "",
+        isImportant: false,
+        date: new Date()
+      };
+    }
+  }
+
+  ok(currentEvent) {
+    if (currentEvent) {
+      if (this.isEditMode) {
+        currentEvent.isImportant = currentEvent.isImportant === "true";
+        this.http.put<CalendarEvent[]>(this.baseUrl + 'api/calendarevent/' + currentEvent.id, currentEvent).subscribe(result => {
+          if (result) {
+            this.refreshTable();
+            alert('success edit');
+          }
+        }, error => console.error(error));
+      } else {
+        currentEvent.isImportant = currentEvent.isImportant === "true" || currentEvent.isImportant === true;
+        currentEvent.id = Guid.create().toString();
+        this.http.post<CalendarEvent[]>(this.baseUrl + 'api/calendarevent', currentEvent).subscribe(result => {
+          if (result) {
+            this.refreshTable();
+            alert('success add');
+          }
+        }, error => console.error(error));
+      }
+
+    }
+  }
+
+  cancel() {
+    this.isEditMode = false;
+    this.currentEvent = null;
+  }
+
+  delete(calendarEvent) {
+    if (confirm('Are you sure you want to delete ' + calendarEvent.name + '?')) {
+      this.http.delete<CalendarEvent[]>(this.baseUrl + 'api/calendarevent/' + calendarEvent.id).subscribe(result => {
+
+        if (result) {
           this.refreshTable();
           alert('success');
         }
       }, error => console.error(error));
     }
-  }
 
-  cancel(){
-    this.isEditMode = false;
-    this.currentEvent = null;
   }
 }
 
 interface CalendarEvent {
+  id: string;
   name: string;
   isImportant: boolean;
   date: Date;
